02450e2ebca5b5dfaea22b37d88f2f5f71dd9ad4
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2022-06-18 00:50:58 +0200

    js: add status items

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,41 @@ import { useState } from 'react';
 import './Main.css';
 import cross from '../../assets/icon-cross.svg';
 
+const OutputItems = (props) => {
+  // console.log(props);
+  // console.log(props.props.formSubmit);
+  var t = 0;
+
+  const test = () => {
+    props.props.formSubmit.forEach((element) => {
+      const a = element.checked;
+      console.log(a);
+
+      if (!a) {
+        t++;
+      } else {
+        return;
+      }
+      return t;
+    });
+  };
+  test();
+
+  return (
+    <div className="status-items">
+      <span className={`status-items__span status-items__span-${props.props.themeClass}`}>{t} items left</span>
+      <div className="status-items__buttons">
+        <button className={`btn-1 status-items__status status-items__status-${props.props.themeClass}`}>All</button>
+        <button className={`btn-2 status-items__status status-items__status-${props.props.themeClass}`}>Active</button>
+        <button className={`btn-2 status-items__status status-items__status-${props.props.themeClass}`}>Completed</button>
+      </div>
+
+      <button className={`status-items__status status-items__status-${props.props.themeClass}`}>Clear Completed</button>
+    </div>
+  );
+};
+
 const OutputFormSubmit = (props) => {
-  // console.log(props.formSubmit);
   const animationHoverOn = (e) => {
     e.style.opacity = '1';
   };
@@ -15,18 +48,19 @@ const OutputFormSubmit = (props) => {
   const rows = props.formSubmit.map((element, index) => {
     return (
       <div
-        className="list__items"
+        className={`list__items list__items-${props.themeClass}`}
         key={index}
-        onMouseEnter={(e) => animationHoverOn(e.currentTarget.childNodes[1])}
-        onMouseLeave={(e) => animationHoverOff(e.currentTarget.childNodes[1])}>
-        <label className={`list__label list__label_${props.themeClass} ${element.checked ? 'checked' : ''}`}>
-          <input
-            type="checkbox"
-            checked={element.checked}
-            className={`list__checkbox list__checkbox_${props.themeClass}`}
-            onChange={() => props.updateForm(index)}
-            name="checkbox"
-          />
+        onMouseEnter={(e) => animationHoverOn(e.currentTarget.childNodes[2])}
+        onMouseLeave={(e) => animationHoverOff(e.currentTarget.childNodes[2])}>
+        <input
+          id={index}
+          type="checkbox"
+          checked={element.checked}
+          className={`list__checkbox list__checkbox_${props.themeClass}`}
+          onChange={() => props.updateForm(index)}
+          name="checkbox"
+        />
+        <label htmlFor={index} className={`list__label list__label_${props.themeClass} ${element.checked ? 'checked' : ''}`}>
           {element.text}
         </label>
         <button className="list__delete" onClick={() => props.removeCharacter(index)}>
@@ -36,7 +70,12 @@ const OutputFormSubmit = (props) => {
     );
   });
 
-  return <div className={`list list_${props.themeClass}`}>{rows}</div>;
+  return (
+    <div className={`list list_${props.themeClass}`}>
+      {rows}
+      <OutputItems props={props} />
+    </div>
+  );
 };
 
 const Main = (props) => {
