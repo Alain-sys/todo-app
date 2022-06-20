30fcb57edc26ee7481814624d4fdeb52e9bbcb31
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2022-06-20 22:19:11 +0200

    js: add status buttons

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,36 +2,45 @@ import { useState } from 'react';
 import './Main.css';
 import cross from '../../assets/icon-cross.svg';
 
-const OutputItems = (props) => {
-  // console.log(props);
-  // console.log(props.props.formSubmit);
-  var t = 0;
+const ActionOutputFormSubmit = (props) => {
+  var countElementChecked = 0;
 
-  const test = () => {
-    props.props.formSubmit.forEach((element) => {
-      const a = element.checked;
-      console.log(a);
+  const countItemsLeft = () => {
+    props.formSubmit.forEach((element) => {
+      const elementChecked = element.checked;
 
-      if (!a) {
-        t++;
+      if (!elementChecked) {
+        countElementChecked++;
       } else {
         return;
       }
-      return t;
+      return countElementChecked;
     });
   };
-  test();
+  countItemsLeft();
 
   return (
     <div className="status-items">
-      <span className={`status-items__span status-items__span-${props.props.themeClass}`}>{t} items left</span>
+      <span className={`status-items__span status-items__span-${props.themeClass}`}>{countElementChecked} items left</span>
       <div className="status-items__buttons">
-        <button className={`btn-1 status-items__status status-items__status-${props.props.themeClass}`}>All</button>
-        <button className={`btn-2 status-items__status status-items__status-${props.props.themeClass}`}>Active</button>
-        <button className={`btn-2 status-items__status status-items__status-${props.props.themeClass}`}>Completed</button>
+        <button className={`btn-1 status-items__status status-items__status-${props.themeClass}`} onClick={(e) => props.statusTodoItems(e.target)}>
+          All
+        </button>
+        <button
+          className={`btn-2 status-items__status status-items__status-${props.themeClass}`}
+          data-id="active"
+          onClick={(e) => props.statusTodoItems(e.target)}>
+          Active
+        </button>
+        <button
+          className={`btn-2 status-items__status status-items__status-${props.themeClass}`}
+          data-id="completed"
+          onClick={(e) => props.statusTodoItems(e.target)}>
+          Completed
+        </button>
       </div>
 
-      <button className={`status-items__status status-items__status-${props.props.themeClass}`}>Clear Completed</button>
+      <button className={`status-items__status status-items__status-${props.themeClass}`}>Clear Completed</button>
     </div>
   );
 };
@@ -45,7 +54,7 @@ const OutputFormSubmit = (props) => {
     e.style.opacity = '0';
   };
 
-  const rows = props.formSubmit.map((element, index) => {
+  const test = (element, index) => {
     return (
       <div
         className={`list__items list__items-${props.themeClass}`}
@@ -63,19 +72,64 @@ const OutputFormSubmit = (props) => {
         <label htmlFor={index} className={`list__label list__label_${props.themeClass} ${element.checked ? 'checked' : ''}`}>
           {element.text}
         </label>
-        <button className="list__delete" onClick={() => props.removeCharacter(index)}>
+        <button className="list__delete" onClick={() => props.removeItems(index)}>
           <img src={cross} alt="cross" />
         </button>
       </div>
     );
-  });
+  };
 
-  return (
-    <div className={`list list_${props.themeClass}`}>
-      {rows}
-      <OutputItems props={props} />
-    </div>
-  );
+  // const rows = props.statusTodo.map((element, index) => {
+  //   return (
+  //     <div
+  //       className={`list__items list__items-${props.themeClass}`}
+  //       key={index}
+  //       onMouseEnter={(e) => animationHoverOn(e.currentTarget.childNodes[2])}
+  //       onMouseLeave={(e) => animationHoverOff(e.currentTarget.childNodes[2])}>
+  //       <input
+  //         id={index}
+  //         type="checkbox"
+  //         checked={element.checked}
+  //         className={`list__checkbox list__checkbox_${props.themeClass}`}
+  //         onChange={() => props.updateForm(index)}
+  //         name="checkbox"
+  //       />
+  //       <label htmlFor={index} className={`list__label list__label_${props.themeClass} ${element.checked ? 'checked' : ''}`}>
+  //         {element.text}
+  //       </label>
+  //       <button className="list__delete" onClick={() => props.removeItems(index)}>
+  //         <img src={cross} alt="cross" />
+  //       </button>
+  //     </div>
+  //   );
+  // });
+
+  // return <>{rows}</>;
+
+  /**************TEST  *******/
+  console.log(props.statusTodo);
+  if (props.statusTodo === 'active') {
+    const rows = props.formSubmit
+      .filter((todo) => todo.checked === false)
+      .map((element, index) => {
+        return test(element, index);
+      });
+    return <>{rows}</>;
+  } else if (props.statusTodo === 'completed') {
+    const rows = props.formSubmit
+      .filter((todo) => todo.checked === true)
+      .map((element, index) => {
+        return test(element, index);
+      });
+    return <>{rows}</>;
+  } else if (props.statusTodo === 'all') {
+    const rows = props.formSubmit.map((element, index) => {
+      return test(element, index);
+    });
+    return <>{rows}</>;
+  } else {
+    return;
+  }
 };
 
 const Main = (props) => {
@@ -90,6 +144,13 @@ const Main = (props) => {
 
   const [initialValue, setInitialValue] = useState('');
   const [formSubmit, setFormSubmit] = useState(todoItems);
+
+  // const [statusTodo, setStatusTodo] = useState(formSubmit);
+  const [statusTodo, setStatusTodo] = useState('all');
+  // const [items, setItems] = useState(formSubmit);
+
+  console.log('form', formSubmit);
+  console.log('status', statusTodo);
 
   const handleChange = (e) => {
     setInitialValue(e);
@@ -108,20 +169,48 @@ const Main = (props) => {
 
   const updateForm = (index) => {
     let items = [...formSubmit];
+    // console.log(items);
     let item = { ...items[index] };
     item.checked = !item.checked;
     items[index] = item;
     setFormSubmit(items);
+    // setStatusTodo(items);
   };
 
-  const removeCharacter = (index) => {
-    const test = formSubmit;
+  const removeItems = (index) => {
+    const element = formSubmit;
 
     setFormSubmit(
-      test.filter((character, i) => {
+      element.filter((items, i) => {
         return i !== index;
       })
     );
+
+    // setStatusTodo(
+    //   element.filter((items, i) => {
+    //     return i !== index;
+    //   })
+    // );
+  };
+
+  const statusTodoItems = (e) => {
+    // if (e.dataset.id === 'active') {
+    //   setStatusTodo(formSubmit.filter((item) => item.checked === false));
+    // } else if (e.dataset.id === 'completed') {
+    //   setStatusTodo(formSubmit.filter((item) => item.checked === true));
+    // } else {
+    //   setStatusTodo(formSubmit);
+    // }
+
+    if (e.dataset.id === 'active') {
+      setStatusTodo('active');
+    } else if (e.dataset.id === 'completed') {
+      setStatusTodo('completed');
+    } else {
+      setStatusTodo('all');
+    }
+
+    // console.log(statusTodo);
   };
 
   return (
@@ -137,7 +226,17 @@ const Main = (props) => {
           placeholder="Create a new todo..."
         />
       </form>
-      <OutputFormSubmit themeClass={props.themeClass} formSubmit={formSubmit} removeCharacter={removeCharacter} updateForm={updateForm} />
+      <div className={`list list_${props.themeClass}`}>
+        <OutputFormSubmit
+          themeClass={props.themeClass}
+          formSubmit={formSubmit}
+          statusTodo={statusTodo}
+          removeItems={removeItems}
+          updateForm={updateForm}
+          statusTodoItems={statusTodoItems}
+        />
+        <ActionOutputFormSubmit themeClass={props.themeClass} formSubmit={formSubmit} statusTodoItems={statusTodoItems} />
+      </div>
     </main>
   );
 };
